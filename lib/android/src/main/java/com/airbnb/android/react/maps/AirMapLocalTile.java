cd94d20359953bad26b8c78c56ae5ffc21a732ea
Jose Espinoza <iespinoza@nuup.co>
2020-03-17 13:20:50 -0700

    Tiles en carpeta Assets de android

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapLocalTile.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapLocalTile.java
@@ -1,6 +1,7 @@
 package com.airbnb.android.react.maps;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Tile;
@@ -14,18 +15,23 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.lang.IllegalArgumentException;
+
 public class AirMapLocalTile extends AirMapFeature {
 
     class AIRMapLocalTileProvider implements TileProvider {
         private static final int BUFFER_SIZE = 16 * 1024;
         private int tileSize;
         private String pathTemplate;
+				private String urlScope = "assets";
+				private AssetManager mAssets;
 
-
-        public AIRMapLocalTileProvider(int tileSizet, String pathTemplate) {
-            this.tileSize = tileSizet;
-            this.pathTemplate = pathTemplate;
-        }
+				public AIRMapLocalTileProvider(int tileSizet, String pathTemplate, String urlScope, AssetManager assets) {
+						mAssets = assets;
+						this.tileSize = tileSizet;
+						this.pathTemplate = pathTemplate;
+						this.urlScope = urlScope;
+				}
 
         @Override
         public Tile getTile(int x, int y, int zoom) {
@@ -41,34 +47,52 @@ public class AirMapLocalTile extends AirMapFeature {
             this.tileSize = tileSize;
         }
 
+				public void setUrlScope(String urlScope) {
+					this.urlScope = urlScope;
+				}
+
         private byte[] readTileImage(int x, int y, int zoom) {
-            InputStream in = null;
-            ByteArrayOutputStream buffer = null;
-            File file = new File(getTileFilename(x, y, zoom));
+      			InputStream in = null;
+        		ByteArrayOutputStream buffer = null;
+						System.out.println(this.urlScope);
 
-            try {
-                in = new FileInputStream(file);
-                buffer = new ByteArrayOutputStream();
+						try {
+								switch (this.urlScope) {
+									case "assets": // by default
+										in = mAssets.open(getTileFilename(x, y, zoom));
+										break;
+									case "fs":
+										File file = new File(getTileFilename(x, y, zoom));
+										in = new FileInputStream(file);
+										break;
+									default:
+										throw new IllegalArgumentException("You have to specify an urlScope");
+								}
+								
+								buffer = new ByteArrayOutputStream();
 
-                int nRead;
-                byte[] data = new byte[BUFFER_SIZE];
+								int nRead;
+								byte[] data = new byte[BUFFER_SIZE];
 
-                while ((nRead = in.read(data, 0, BUFFER_SIZE)) != -1) {
-                    buffer.write(data, 0, nRead);
-                }
-                buffer.flush();
+								while ((nRead = in.read(data, 0, BUFFER_SIZE)) != -1) {
+										buffer.write(data, 0, nRead);
+								}
+								buffer.flush();
 
-                return buffer.toByteArray();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            } catch (OutOfMemoryError e) {
-                e.printStackTrace();
-                return null;
-            } finally {
-                if (in != null) try { in.close(); } catch (Exception ignored) {}
-                if (buffer != null) try { buffer.close(); } catch (Exception ignored) {}
-            }
+								return buffer.toByteArray();
+						} catch (IOException e) {
+								e.printStackTrace();
+								return null;
+						} catch (OutOfMemoryError e) {
+								e.printStackTrace();
+								return null;
+						} catch (IllegalArgumentException e) {
+								e.printStackTrace();
+								return null;
+						} finally {
+								if (in != null) try { in.close(); } catch (Exception ignored) {}
+								if (buffer != null) try { buffer.close(); } catch (Exception ignored) {}
+						}
         }
 
         private String getTileFilename(int x, int y, int zoom) {
@@ -85,6 +109,7 @@ public class AirMapLocalTile extends AirMapFeature {
     private AirMapLocalTile.AIRMapLocalTileProvider tileProvider;
 
     private String pathTemplate;
+		private String urlScope;
     private float tileSize;
     private float zIndex;
 
@@ -116,6 +141,13 @@ public class AirMapLocalTile extends AirMapFeature {
         }
     }
 
+		public void setUrlScope(String urlScope) {
+			this.urlScope = urlScope;
+			if (tileProvider != null) {
+				tileProvider.setUrlScope(urlScope);
+			}
+		}
+
     public TileOverlayOptions getTileOverlayOptions() {
         if (tileOverlayOptions == null) {
             tileOverlayOptions = createTileOverlayOptions();
@@ -126,7 +158,7 @@ public class AirMapLocalTile extends AirMapFeature {
     private TileOverlayOptions createTileOverlayOptions() {
         TileOverlayOptions options = new TileOverlayOptions();
         options.zIndex(zIndex);
-        this.tileProvider = new AirMapLocalTile.AIRMapLocalTileProvider((int)this.tileSize, this.pathTemplate);
+        this.tileProvider = new AirMapLocalTile.AIRMapLocalTileProvider((int)this.tileSize, this.pathTemplate, this.urlScope, getResources().getAssets());
         options.tileProvider(this.tileProvider);
         return options;
     }
